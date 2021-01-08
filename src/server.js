140a5be87842601b6aae563d2c404a5bdf24c36a
Luã Mourão <luan.gcc@gmail.com>
2021-01-08 16:35:53 -0300

    Refatoração: arquivos estáticos e server.js

--- src/server.js
+++ src/server.js
@@ -1,27 +1,23 @@
-// servidor
 const express = require('express');
 const server = express();
 
 const { pageLanding, pageTreinar, pageDarAula, saveClasses } = require('./pages')
 
-// configurar nunjucks (template engine)
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
   express: server,
   noCache: true,
 })
 
-// início e configuração do servidor
 server
-// configuração para receber dados do req.body
-.use(express.urlencoded({ extended: true }))
-// configurar arquivos estáticos (css, scripts, imagens)
+.use(express.urlencoded({ extended: true })) // config req.body
 .use(express.static('public'))
-// rotas da aplicação
+
+// app routes
 .get("/", pageLanding)
 .get("/treinar", pageTreinar)
 .get("/dar-aula", pageDarAula)
 .post("/save-classes", saveClasses)
-// start do servidor
+
 .listen(5500)
 
